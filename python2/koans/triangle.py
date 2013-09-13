ec15a47d128c72605fabdd1db7853ef60130078d
Anderson Mesquita <andersonvom@thoughtworks.com>
2013-09-13 16:20:23 -0500

    Thinking AboutTriangleProject: DONE

--- python2/koans/triangle.py
+++ python2/koans/triangle.py
@@ -18,8 +18,11 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
-    # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+    unique_sides = set([a, b, c])
+    num_sides = len(unique_sides)
+    if num_sides == 1: return 'equilateral'
+    if num_sides == 2: return 'isosceles'
+    return 'scalene'
 
 
 # Error class used in part 2.  No need to change this code.
