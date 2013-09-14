86698fede0282bfe80d1853c9fb6057112180c0e
Anderson Mesquita <andersonvom@thoughtworks.com>
2013-09-13 22:26:54 -0500

    Thinking AboutTriangleProject2: DONE

--- python2/koans/triangle.py
+++ python2/koans/triangle.py
@@ -18,7 +18,18 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
-    unique_sides = set([a, b, c])
+    sides = sorted([a, b, c])
+    invalid_side = (sides[0] <= 0)
+    smaller_sides = sides[0] + sides[1]
+    side_too_big = (smaller_sides <= sides[2])
+    if invalid_side:
+        raise TriangleError, "Triangles cannot have negative sides"
+    if side_too_big:
+        error_msg = "Longest side ({0}) has to be"
+        "less than {1}".format(sides[2], smaller_sides)
+        raise TriangleError, "One of the sides of this triangle ({0}) is too big"
+
+    unique_sides = set(sides)
     num_sides = len(unique_sides)
     if num_sides == 1: return 'equilateral'
     if num_sides == 2: return 'isosceles'
